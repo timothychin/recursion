d6eed2939637d8c858e3c355b65efcc858a33909
Tim Chin <timchin@Tims-MacBook-Pro.local>
2016-10-03 20:15:58 -0700

    -Added pseudocode

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,22 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-
-// but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //new var StringArray to store object/array cases
+//EXCEPTION CASE
+  //Check for undefined, null, symbol
+    //Return 'null'
+// !! ------ !! Possible area to fix prop issue
+//RECURSIVE CASE - only needed if obj is an object or array
+  //Check for Array
+    //For loop through array
+      //Push stringify(array[i]) to StringArray
+    //return StringArray joined with commas and opened/closed with square brackets
+
+  //Check for object + !null
+    //for in loop on obj
+      //if stringify(prop) !null
+        //push stringify(prop) + ":" + stringify(value) to StringArray
+    //return StringArray with commas and opened/closed with curly fries
+
+//BASE CASE
+  //Return primitive values as string or string with extra quotations
 };
