a06c0f35003ce1656209d682d53a8b1db6b080ad
Tim Chin <timchin@Tims-MacBook-Pro.local>
2016-10-06 14:39:01 -0700

    Cleaned up excess code

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,24 +1,21 @@
 var stringifyJSON = function(obj) {
-  var keyValuePairs = [];
-  //EXCEPTION CASE
-  if(typeof obj === "symbol"  ||  obj === undefined  ||  typeof obj === 'function'){
-    return;
-  }
+  var pairs = [];
   //RECURSIVE CASE - only needed if obj is an object or array
-  if(Array.isArray(obj) === true){
-    for(var i = 0; i < obj.length; i++){
-      keyValuePairs.push(stringifyJSON(obj[i]));
+  if( Array.isArray(obj) ){
+    for(  var i = 0; i < obj.length; i++  ){
+      pairs.push( stringifyJSON(obj[i]) );
     }
-    return '[' + keyValuePairs.join(',') + ']';
+    return '[' + pairs.join(',') + ']';
   }
-  if(typeof obj === 'object'  &&  Array.isArray(obj) === false  &&  obj !== null){
-    for(var prop in obj){
-      if(stringifyJSON(obj[prop]) !== undefined){
-        keyValuePairs.push(stringifyJSON(prop) + ':' + stringifyJSON(obj[prop]));
+  if( typeof obj === 'object'  &&  obj ){
+    for(  var prop in obj  ){
+      if( obj[prop] === undefined || typeof obj[prop] === 'function' ){
+        continue;
       }
+      pairs.push( stringifyJSON(prop) + ':' + stringifyJSON(obj[prop])   );
     }
-    return '{' + keyValuePairs.join(',') + '}';
+    return '{' + pairs.join(',') + '}';
   }
   //BASE CASE
-  return typeof obj === 'string' ? '"' + obj + '"' : String(obj);
+  return typeof obj === 'string' ? '"' + obj + '"' : ''+obj;
 };
