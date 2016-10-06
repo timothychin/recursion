190dee1ae2967e74a3bd526bd4d0658e6ad69d4e
Tim Chin <timchin@Tims-MacBook-Pro.local>
2016-10-05 23:37:07 -0700

    stringifyJSON passes all tests YAY

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,37 +2,24 @@
 var stringifyJSON = function(obj) {
   var keyValuePairs = [];
 //EXCEPTION CASE
-  if(obj === null || typeof obj === "symbol"){
-    return 'null';
+  if(typeof obj === "symbol" || obj === undefined || typeof obj === 'function'){
+    return;
   }
-// !! ------ !! Possible area to fix prop issue
 //RECURSIVE CASE - only needed if obj is an object or array
-
   if(Array.isArray(obj) === true){
     for(var i = 0; i < obj.length; i++){
       keyValuePairs.push(stringifyJSON(obj[i]));
     }
     return '[' + keyValuePairs.join(',') + ']';
   }
-
-  if(typeof obj === 'object' && Array.isArray(obj) === false){
+  if(typeof obj === 'object' && Array.isArray(obj) === false && obj !== null){
     for(var prop in obj){
-      if(stringify(obj[prop]))
-      if(stringifyJSON(prop) !== 'null'){
+      if(stringifyJSON(obj[prop]) !== undefined){
         keyValuePairs.push(stringifyJSON(prop) + ':' + stringifyJSON(obj[prop]));
       }
     }
     return '{' + keyValuePairs.join(',') + '}';
   }
-
 //BASE CASE
-  return typeof obj === 'string' ? '"' + obj + '"' : obj.toString();
+  return typeof obj === 'string' ? '"' + obj + '"' : String(obj);
 };
-
-
-/* NOTES
-
-- JSON does not allow you to stringify functions or undefined values, so you should skip those key/value pairs.
--
-
-*/
