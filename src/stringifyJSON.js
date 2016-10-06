11f699abc5deeb918be46d06c43cfe90cd9c3238
Tim Chin <timchin@Tims-MacBook-Pro.local>
2016-10-05 20:48:46 -0700

    stringifyJSON passes all but one test case doesn't omit functions and undefined in objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,22 +1,39 @@
 
 var stringifyJSON = function(obj) {
-  //new var StringArray to store object/array cases
+  var keyValuePairs = [];
 //EXCEPTION CASE
-  //Check for undefined, null, symbol
-    //Return 'null'
+  if(obj === undefined || obj === null || typeof obj === "symbol"){
+    return 'null';
+  }
 // !! ------ !! Possible area to fix prop issue
 //RECURSIVE CASE - only needed if obj is an object or array
-  //Check for Array
-    //For loop through array
-      //Push stringify(array[i]) to StringArray
-    //return StringArray joined with commas and opened/closed with square brackets
 
-  //Check for object + !null
-    //for in loop on obj
-      //if stringify(prop) !null
-        //push stringify(prop) + ":" + stringify(value) to StringArray
-    //return StringArray with commas and opened/closed with curly fries
+  if(Array.isArray(obj) === true){
+    for(var i = 0; i < obj.length; i++){
+      keyValuePairs.push(stringifyJSON(obj[i]));
+    }
+    return '[' + keyValuePairs.join(',') + ']';
+  }
+
+  if(typeof obj === 'object' && Array.isArray(obj) === false){
+    for(var prop in obj){
+      if(stringifyJSON(prop) !== 'null' && typeof stringifyJSON(obj[prop]) !== 'function' && stringifyJSON(obj[prop]) != undefined){
+        keyValuePairs.push(stringifyJSON(prop) + ':' + stringifyJSON(obj[prop]));
+      }
+    }
+    return '{' + keyValuePairs.join(',') + '}';
+  }
 
 //BASE CASE
-  //Return primitive values as string or string with extra quotations
+  return typeof obj === 'string' ? '"' + obj + '"' : obj.toString();
 };
+
+
+/*NOTES/STRAY THOUGHTS
+
+-NEEDS to assess which when a key:value pair in an object should be omitted
+-exception case returns nothing, for in loop checks  if stringify(prop) exists, ISSUE value needs to exist and return null,
+-undefined, functions, and symbols are omitted in an object, censored to null in an array
+
+
+*/
