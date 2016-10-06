787d470d3ec6f62f678affcf4312b4ca22bf6689
Tim Chin <timchin@Tims-MacBook-Pro.local>
2016-10-05 23:52:55 -0700

    Edited styling

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,18 +1,17 @@
-
 var stringifyJSON = function(obj) {
   var keyValuePairs = [];
-//EXCEPTION CASE
-  if(typeof obj === "symbol" || obj === undefined || typeof obj === 'function'){
+  //EXCEPTION CASE
+  if(typeof obj === "symbol"  ||  obj === undefined  ||  typeof obj === 'function'){
     return;
   }
-//RECURSIVE CASE - only needed if obj is an object or array
+  //RECURSIVE CASE - only needed if obj is an object or array
   if(Array.isArray(obj) === true){
     for(var i = 0; i < obj.length; i++){
       keyValuePairs.push(stringifyJSON(obj[i]));
     }
     return '[' + keyValuePairs.join(',') + ']';
   }
-  if(typeof obj === 'object' && Array.isArray(obj) === false && obj !== null){
+  if(typeof obj === 'object'  &&  Array.isArray(obj) === false  &&  obj !== null){
     for(var prop in obj){
       if(stringifyJSON(obj[prop]) !== undefined){
         keyValuePairs.push(stringifyJSON(prop) + ':' + stringifyJSON(obj[prop]));
@@ -20,6 +19,6 @@ var stringifyJSON = function(obj) {
     }
     return '{' + keyValuePairs.join(',') + '}';
   }
-//BASE CASE
+  //BASE CASE
   return typeof obj === 'string' ? '"' + obj + '"' : String(obj);
 };
