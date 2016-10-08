4d21eee33b5c40b18405e28144d3ffd27a7bf102
Tim Chin <timchin@Tims-MacBook-Pro.local>
2016-10-08 10:54:38 -0700

    Cleaned up stringifyJSON code

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,6 +1,5 @@
 var stringifyJSON = function(obj) {
   var pairs = [];
-  //RECURSIVE CASE - only needed if obj is an object or array
   if( Array.isArray(obj) ){
     for(  var i = 0; i < obj.length; i++  ){
       pairs.push( stringifyJSON(obj[i]) );
@@ -9,13 +8,11 @@ var stringifyJSON = function(obj) {
   }
   if( typeof obj === 'object'  &&  obj ){
     for(  var prop in obj  ){
-      if( obj[prop] === undefined || typeof obj[prop] === 'function' ){
-        continue;
+      if( obj[prop] !== undefined && typeof obj[prop] !== 'function' ){
+        pairs.push( stringifyJSON(prop) + ':' + stringifyJSON(obj[prop])   );
       }
-      pairs.push( stringifyJSON(prop) + ':' + stringifyJSON(obj[prop])   );
     }
     return '{' + pairs.join(',') + '}';
   }
-  //BASE CASE
   return typeof obj === 'string' ? '"' + obj + '"' : ''+obj;
 };
