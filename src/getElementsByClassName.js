63e0f5fc07337202d9cbeba6bacc9f63c7240d4f
Tim Chin <timchin@Tims-MacBook-Pro.local>
2016-10-08 10:15:12 -0700

    Should work for single class inputs

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,10 +1,62 @@
-// If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
+//
 
-// But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var foundClasses = [];
+  //Recursive case needs to pass a node and not classname, so nesting a function allows me to make a recursive call with a different input
+  var searchNodes = function(node){
+    var classNames =
+    if( node.classLists === className){
+      foundClasses.push(node);
+    }
+    for(var i = 0; i < node.childNodes.length; i++){
+      searchNodes(node.childNodes[i]);
+    }
+  }
+  searchNodes(document.body);
+  return foundClasses;
 };
+
+
+var getElementsByClassName = function(className){
+  //var passedNodes
+  //className = className.split(' ') OR new var
+  //var new function, takes in a node
+    //Runs through
+      //if node matches class name
+        //save to passedNodes
+    //for loop to iterate through all childNodes
+      //call function on childNodes[i]
+  //function on document.body
+  //return passedNodes
+}
+
+
+/*
+var searchNodes  = function(node){
+    //Needs to match
+    var nodeList   = node.classLists;
+    var checkTicks = 0;
+    //Compare this branch class to className
+    //Parse class names
+    for( var k = 0; k < args.length; k++ ){
+      //Parse through node classLists to match
+      for( var j = 0; j < nodeList.length; j++){
+        if(args[k] === nodeList[j]){
+          foundClasses.push(nodeList[j]);
+          checkTicks++;
+        }
+      }
+      //If matches, saves node / checkTick ++
+    }
+    if(checkTicks === args.length){
+      return
+    }
+    //if checkTick === args.length
+      //
+    //Iterate childNodes
+    for( var i = 0; i < node.childNodes.length; i++ ){
+      //searchNodes on each childNode
+      searchNodes(node.childNodes[i]);
+    }
+  }
+*/
